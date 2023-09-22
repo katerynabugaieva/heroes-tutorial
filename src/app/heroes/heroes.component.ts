6d703e141923d2cbad29ca6ee5205c0c870f3203
Kateryna Bugaieva <sonnensmile@gmail.com>
2023-09-22 13:27:35 +0200

    logo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,24 @@
-import { HeroService } from './../hero.service';
-import { HeroDetailComponent } from './../hero-detail/hero-detail.component';
-import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { Hero } from '../hero';
-import { MessageService } from '../message.service';
+import { HeroService } from '../hero.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [CommonModule, FormsModule, HeroDetailComponent],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css'],
+  standalone: true, 
+  imports: [RouterLink, CommonModule]
 })
-export class HeroesComponent {
-  selectedHero?: Hero;
-
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
