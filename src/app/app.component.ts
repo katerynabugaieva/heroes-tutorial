d450a29b31b00d7c8507dee93cd07e09aaad939d
Kateryna Bugaieva <sonnensmile@gmail.com>
2023-10-02 13:25:42 +0200

    logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { MessagesComponent } from './messages/messages.component';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, MessagesComponent, RouterLink, RouterLinkActive ],
+  imports: [RouterModule, RouterOutlet, HeroesComponent, MessagesComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'run-hannover';
+  title = 'heroes-tutorial';
 }
