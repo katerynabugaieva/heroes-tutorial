fbb8df84f5368a155baf6ebc1026402825704ebd
Kateryna Bugaieva <sonnensmile@gmail.com>
2024-02-23 08:54:31 +0100

    logo

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
