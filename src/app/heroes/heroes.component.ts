48eb2100411fe7d3b05824c90999e91e47c704a4
Kateryna Bugaieva <sonnensmile@gmail.com>
2024-04-30 14:43:39 +0200

    change

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,28 +1,28 @@
-import { RouterLink } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
 
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-import { CommonModule } from '@angular/common';
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  standalone: true, 
-  imports: [RouterLink, CommonModule]
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
+    console.log("here is a change");
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
